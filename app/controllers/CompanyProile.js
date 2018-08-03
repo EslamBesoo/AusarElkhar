7e536e2f864a2f93fce59003dc3b8791149a1487
EslamBesoo <Islam.Moh.Abd.Mohamed@foc.cu.edu.eg>
2018-08-04 00:41:16 +0200

    edit company

--- app/controllers/CompanyProile.js
+++ app/controllers/CompanyProile.js
@@ -1,6 +1,6 @@
 // Arguments passed into this controller can be accessed via the `$.args` object directly or:
 var args = $.args;
-
+//infoo
 $.form.addView(Alloy.createController("view/companyProfileView",args.data).getView());
 $.form.addView(Alloy.createController("view/itemLIstView",args).getView());
 
